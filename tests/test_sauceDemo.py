d0d7ace9be08d1bd09f0b3b9525ddc407d1808d6
nkosi <nkosi.cele@simplify.africa>
2024-12-09 20:33:24 +0200

    implemented the code tologin

--- tests/test_sauceDemo.py
+++ tests/test_sauceDemo.py
@@ -3,6 +3,7 @@ import time
 import allure
 import pytest
 
+from Pages.loginPage import LoginPage
 from Utils.ReadProperties_loginDetails import ReadLoginProperties
 
 
@@ -13,4 +14,11 @@ class Test_SauceDemo:
     @allure.severity(allure.severity_level.CRITICAL)
     def test_loginToSauceDemo(self, setup):
         self.driver = setup
+        self.driver.get(self.sauceDemoURL)
+        self.driver.maximize_window()
+        self.login = LoginPage(self.driver)
+        self.login.enterUsername("standard_user")
+        self.login.enterPassword("secret_sauce")
+        self.login.clickLoginButton()
+
         time.sleep(3)

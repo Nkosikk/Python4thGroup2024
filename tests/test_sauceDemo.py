c87c5ee4e13a3e1a3e1e4239fde432765292b0a5
Simo20202 <simomkhasibe@yahoo.com>
2024-12-11 20:02:03 +0200

    Added YourCart Page

--- tests/test_sauceDemo.py
+++ tests/test_sauceDemo.py
@@ -6,6 +6,7 @@ from allure_commons.types import AttachmentType
 
 from Pages.homePage import HomePage
 from Pages.loginPage import LoginPage
+from Pages.yourCartPage import YourCartPage
 from Utils.ReadProperties_loginDetails import ReadLoginProperties
 
 
@@ -22,12 +23,17 @@ class Test_SauceDemo:
         self.driver.maximize_window()
         self.login = LoginPage(self.driver)
         self.home = HomePage(self.driver)
+        self.yourCart = YourCartPage(self.driver)
         self.login.enterUsername(self.username)
         self.login.enterPassword(self.password)
         allure.attach(self.driver.get_screenshot_as_png(),name="login page",attachment_type=AttachmentType.PNG)
         self.login.clickLoginButton()
         self.home.verifyThatUserIsLoggedIn()
         allure.attach(self.driver.get_screenshot_as_png(), name="Inventory page", attachment_type=AttachmentType.PNG)
+        self.home.selectBackPack()
+        self.yourCart.viewToCart()
+        self.yourCart.clickCheckoutButton()
+
 
 
         time.sleep(3)
